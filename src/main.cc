2d0a3680c9a526ec57cc09e63e3355f39f3e1170
Bradley Clemetson <brad@codeprogrammers.net>
2012-03-28 08:34:24 -0700

    Moved from C (just GTK) to GTKmm (C++ Bindings)

---
+++ src/main.cc
@@ -0,0 +1,63 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.cc
+ * Copyright (C) Bradley Clemetson 2012 <brad@codeprogrammers.net>
+ * 
+system-config-server is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * system-config-server is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
+#include <gtkmm.h>
+#include <iostream>
+
+#include "config.h"
+
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#endif
+
+
+
+/* For testing propose use the local (not installed) ui file */
+/* #define UI_FILE PACKAGE_DATA_DIR"/system_config_server/ui/system_config_server.ui" */
+#define UI_FILE "src/system_config_server.ui"
+
+   
+int
+main (int argc, char *argv[])
+{
+	Gtk::Main kit(argc, argv);
+
+
+	//Load the Glade file and instiate its widgets:
+	Glib::RefPtr<Gtk::Builder> builder;
+	try
+	{
+		builder = Gtk::Builder::create_from_file(UI_FILE);
+	}
+	catch (const Glib::FileError & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return 1;
+	}
+	Gtk::Window* main_win = 0;
+	builder->get_widget("main_window", main_win);
+
+
+	if (main_win)
+	{
+		kit.run(*main_win);
+	}
+	return 0;
+}
