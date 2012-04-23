4cc1d9dd0684fddba421cb4cb5e20fdd97e9fb38
Bradley Clemetson <brad@codeprogrammers.net>
2012-04-22 21:20:00 -0700

    If Module has not been initialized properly, g_error will throw.

--- src/ModuleSubsystem/ServiceModuleMain.cc
+++ src/ModuleSubsystem/ServiceModuleMain.cc
@@ -3,16 +3,23 @@
 
 ServiceModule::ServiceModule()
 {
-	g_message("Service Module Default Constructor STUB!\n");
+	g_message("Service Module Default Constructor!\n");
+	mainView = NULL;
 }
 
 void ServiceModule::ShowServiceModule()
 {
 	g_message("Clicked Module: %s", serviceName.c_str());
-	if(rootServiceWindow != NULL)
+	if(mainView != NULL)
 	   {
+		   //Remove any Widget if there is one
 		   rootServiceWindow->remove();
+		   //Load the selected widget
 		   rootServiceWindow->add(*mainView);
 	   }
+	else
+	{
+		g_error("No Glade/Widgets loaded from Module! (ServiceModuleMain::ShowServiceModule)");
+	}
 	
 }
